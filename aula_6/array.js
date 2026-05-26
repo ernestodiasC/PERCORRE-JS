59bb44e24812f9c487c0d6af5820ef4efcf15f43
Ernesto Dias Cecilio <ernestodiascecilio@gmail.com>
2026-05-26 15:32:20 -0300

    Aula 6

---
+++ aula_6/array.js
@@ -0,0 +1,26 @@
+/*
+( = Parênteses
+[ = Colchetes
+{ = Chaves
+*/
+let aGradusIntegrantes=["Ernesto","Yuri","Dieivan","Fabricio","Felipe"]
+let aGradus=[
+    aGradusIntegrantes,
+    [0,3,4,1,2],
+    [0], // JS
+    [3,4], // CSS
+    [1,2], //HTML
+]
+console.log(aGradus)
+
+// COMENTARIOS DO ERMESON BARROS
+//toString() converte um array em uma string
+// join() que converte o array em string
+//  pop() remove o último elemento do array
+// push() insere um elemento no final do array
+// shift() remove o elemento no início do array deslocando todos os 
+// elementos em uma posição
+// unshift() insere um elemento no início do array]
+
+console.log(aGradus.toString())
+console.log(aGradusIntegrantes.join(' | '))
---
+++ aula_6/array2.js
@@ -0,0 +1,5 @@
+var a1=["1",2,3]
+a2=a1
+a2.pop()
+console.log(a1)
+console.log(a2)
